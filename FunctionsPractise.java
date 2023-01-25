92c900462f90697e58a3e06f0c2edb1dea48ce82
Aanchal59 <aanchalnegi59@gmail.com>
2023-01-25 23:45:08 +0530

    Update FunctionsPractise.java

--- FunctionsPractise.java
+++ FunctionsPractise.java
@@ -1,3 +1,5 @@
+//ADD TWO NUMBERS
+
 public class Functions {
 	public static int calculateSum(int a, int b){
 	int sum = a + b;
@@ -9,5 +11,58 @@ public class Functions {
 	Scanner sc =new Scanner(System.in);
 	int a = sc.nextInt();
 	int b = sc.nextInt();
+
+	int sum = calculateSum(a,b);
+	System.out.println(sum);
+
 	}
 }
+
+
+//MULTIPLY 2 NUMBERS AND RETURN PRODUCT
+
+
+public class Functions {
+	public static int calculateProduct(int a, int b){
+	int sum = a * b;
+	return a * b;
+
+	}
+
+	public static void main(String args[]){
+	Scanner sc =new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+
+	System.out.println(calculateProduct(a,b));
+
+	}
+}
+
+//FACTORIAL OF A NUMBER
+
+
+public class Functions {
+	public static void printFactorial(int n){
+		int fact = 1;
+
+		for(int i = n; i>=1; i--){
+			fact = fact * i; 
+		}
+		
+		System.out.println(fact);
+		return;
+	}
+
+	}
+
+	public static void main(String args[]){
+	Scanner sc =new Scanner(System.in);
+	int n = sc.nextInt();
+
+	printFactorial(n);
+	
+
+	}
+}
+
